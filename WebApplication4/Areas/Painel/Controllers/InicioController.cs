1d4b54d19f7be2b76ee0f0c36de5c299f16e85f0
HenriquePRDC <henrique_prdc@hotmail.com>
2014-03-19 21:27:09 -0400

    commit inicial

---
+++ WebApplication4/Areas/Painel/Controllers/InicioController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace WebApplication4.Areas.Painel.Controllers
+{
+    public class InicioController : Controller
+    {
+        public string NameUser { get; set; }
+        //
+        // GET: /Painel/Inicio/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string Nome, string Senha)
+        {
+
+            if (Nome == "Henrique" && Senha == "123")
+            {
+                NameUser = Nome;
+                return RedirectToAction("DadosCadastrais");
+            }
+            return View();
+        }
+        public ActionResult DadosCadastrais()
+        {
+            return View();
+        }
+    }
+    
+}
